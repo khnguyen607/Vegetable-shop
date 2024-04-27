2ac15a91f55909de6f7480d54133f172a924e5f7
khnguyen607 <khnguyen.job@gmail.com>
2024-04-27 18:33:59 +0700

    home & products

--- frontEnd/client/pages/products/products.js
+++ frontEnd/client/pages/products/products.js
@@ -1,40 +1,25 @@
 document.addEventListener('DOMContentLoaded', async () => {
     // Đổ danh sách sản phẩm
     _init();
-    // Đổ danh sách categories
-    _getCategories();
 })
 
 async function _init() {
     var items = await Helper.fetchData("product")
-    showProducts(items)
+    await showProducts(items)
     // Thêm sự kiện lọc giá khi bấm lọc
-    document.querySelector(".price-range-area .text-uppercase").addEventListener('click', () => filterBy())
     document.querySelector(".shop_sidebar_searchbar i").addEventListener('click', () => filterBy())
-}
-
-async function _getCategories() {
-    var items = await Helper.fetchData(`category`)
-    var categories = document.querySelector(".blog_category_item .form-check")
-    categories.innerHTML = ""
-    items.forEach(item => {
-        var category = document.createElement("div");
-        category.classList.add("col");
-        category.innerHTML = `
-            <input class="form-check-input" type="checkbox" value="" id="_categoryID${item.ID}">
-            <label class="form-check-label ms-2" for="_categoryID${item.ID}">
-                ${item.Name}
-            </label>
-        `
-        categories.appendChild(category)
-    });
+    
+    // Kiểm tra xem có biến q không thì lọc ra sản phẩm tương ứng
+    if (Helper.getParameterByName("q")) {
+        document.querySelector(".shop_sidebar_searchbar input").value = Helper.getParameterByName("q")
+        filterBy()
+    }
 }
 
 async function filterBy() {
     document.querySelectorAll("#grid_layout .row.g-4 .col-sm-6").forEach(item => {
         if (
-            byPrice(item.querySelector(".sale_price"))
-            && byName(item.querySelector(".product_title a").textContent)
+            byName(item.querySelector(".product_title a").textContent)
         ) {
             item.classList.remove("d-none")
         } else {
@@ -42,20 +27,6 @@ async function filterBy() {
         }
     })
 
-    // Lọc theo giá
-    function byPrice(value) {
-        var str = document.querySelector("#amount").value;
-        var cleanedStr = str.replace(/₫/g, '');
-        var parts = cleanedStr.split(' - ');
-        var minValue = parseFloat(parts[0].replace(/\./g, '').replace(/,/g, '.'));
-        var maxValue = parseFloat(parts[1].replace(/\./g, '').replace(/,/g, '.'));
-        value = parseFloat(value.textContent.replace(/\./g, '').replace(/,/g, '.'))
-        if ((value < minValue) || (value > maxValue)) {
-            return false
-        } else {
-            return true
-        }
-    }
 
     // Lọc theo tên
     function byName(name) {
